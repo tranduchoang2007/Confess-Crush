ac5dff3c986fe5228fabc142a819d27093d6bacc
tranduchoang2007 <92874068+tranduchoang2007@users.noreply.github.com>
2022-01-24 22:09:27 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     titleWeb: "Tán Crush",
-    introTitle: 'Vũ Thùy Linh',
+    introTitle: 'Thu Thanh ❤',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
