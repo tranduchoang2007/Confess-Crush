6c37999ebe37ccb57b771faf609e0afe431f6999
tranduchoang2007 <92874068+tranduchoang2007@users.noreply.github.com>
2022-01-31 13:32:40 +0700

    Update config.js

--- config.js
+++ config.js
@@ -11,10 +11,10 @@ const CONFIG = {
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
+    btnReply: 'Gửi cho bạn <3', 
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
+    messDesc: 'Tối nay 8h, mình qua đón đi chơi nha.',
+    btnAccept: 'Okiiiii lun ❤ <3',
     messLink: 'https://m.me/tranhoang007'
 }
